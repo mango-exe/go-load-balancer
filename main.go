c5df1a28e3ba45e7a1f2b624646cb77d0091177c
albert-emilian <albert.ion@nextlab.tech>
2024-06-27 20:01:38 +0300

    code refactorization

--- main.go
+++ main.go
@@ -3,82 +3,86 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"strings"
-	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
-var (
-	current int
-	mutex   sync.Mutex
-)
+/*
+ TODO:
+ - health checks for servers
+ - handle tls connections for servers
+ - rate limit
+ - least connections load balance algorithm
+ - command line configuration for load balancer
+*/
 
-type StickySession struct {
-	cookies []*http.Cookie
+type LoadBalancer struct {
+	ctx                  *gin.Context
+	urls                 []*url.URL
+	current              int
+	currentUrl           *url.URL
+	stickySessionEnabled bool
 }
 
-func proxyRequest(urls []*url.URL) gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		existingCookie, err := ctx.Request.Cookie("sticky-session")
+func (l *LoadBalancer) stickySession() {
+	existingCookie, err := l.ctx.Request.Cookie("sticky-session")
 
-		currentUrl := roundRobin(urls)
+	currentUrl := l.roundRobin()
+
+	if err != nil {
+		id := uuid.New()
+		cookieValue := fmt.Sprintf("%s@%s", id, currentUrl.String())
+		encodedValue := base64.RawStdEncoding.EncodeToString([]byte(cookieValue))
+
+		l.ctx.SetCookie("sticky-session", encodedValue, 3600, "", "", false, true)
+	} else {
+		decodedCookieBytes, err := base64.RawStdEncoding.DecodeString(existingCookie.Value)
 
 		if err != nil {
-			id := uuid.New()
-			cookieValue := fmt.Sprintf("%s@%s", id, currentUrl.String())
-			encodedValue := base64.RawStdEncoding.EncodeToString([]byte(cookieValue))
+			fmt.Println(err)
+		}
 
-			ctx.SetCookie("sticky-session", encodedValue, 3600, "", "", false, true)
-		} else {
-			decodedCookieBytes, err := base64.RawStdEncoding.DecodeString(existingCookie.Value)
+		splitCookieValue := strings.Split(string(decodedCookieBytes), "@")
+
+		if len(splitCookieValue) == 2 {
+			_, rawUrl := splitCookieValue[0], splitCookieValue[1]
+
+			parsedUrl, err := url.Parse(rawUrl)
 
 			if err != nil {
 				fmt.Println(err)
 			}
 
-			splitCookieValue := strings.Split(string(decodedCookieBytes), "@")
-
-			if len(splitCookieValue) == 2 {
-				_, rawUrl := splitCookieValue[0], splitCookieValue[1]
-
-				currentUrl, err = url.Parse(rawUrl)
-
-				if err != nil {
-					fmt.Println(err)
-				}
-			}
+			l.currentUrl = parsedUrl
+		} else {
+			l.roundRobin()
 		}
-
-		proxy := httputil.NewSingleHostReverseProxy(currentUrl)
-		ctx.Request.URL.Host = currentUrl.Host
-		ctx.Request.URL.Scheme = currentUrl.Scheme
-		ctx.Request.Header.Set("X-Forwarded-Host", ctx.Request.Host)
-		ctx.Request.Host = currentUrl.Host
-
-		proxy.ServeHTTP(ctx.Writer, ctx.Request)
 	}
 }
 
-func roundRobin(urls []*url.URL) *url.URL {
-	mutex.Lock()
-	defer mutex.Unlock()
+func (l *LoadBalancer) proxyRequest() {
+	proxy := httputil.NewSingleHostReverseProxy(l.currentUrl)
+	l.ctx.Request.URL.Host = l.currentUrl.Host
+	l.ctx.Request.URL.Scheme = l.currentUrl.Scheme
+	l.ctx.Request.Header.Set("X-Forwarded-Host", l.ctx.Request.Host)
+	l.ctx.Request.Host = l.currentUrl.Host
 
-	current = (current + 1) % len(urls)
-	fmt.Println(current)
-	return urls[current]
+	proxy.ServeHTTP(l.ctx.Writer, l.ctx.Request)
 }
 
-func main() {
-	var rawURLs = []string{"http://127.0.0.1:8081", "http://127.0.0.1:8082"}
+func (l *LoadBalancer) roundRobin() *url.URL {
+	l.current = (l.current + 1) % len(l.urls)
+	return l.urls[l.current]
+}
 
+func (l *LoadBalancer) parseUrls(rawUrls []string) {
 	var urls []*url.URL
 
-	for _, rawUrl := range rawURLs {
+	for _, rawUrl := range rawUrls {
 		parsedUrl, err := url.Parse(rawUrl)
 
 		if err != nil {
@@ -87,9 +91,35 @@ func main() {
 		urls = append(urls, parsedUrl)
 	}
 
+	l.urls = urls
+}
+
+func (l *LoadBalancer) handleRequest() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		l.ctx = ctx
+
+		if l.stickySessionEnabled {
+			l.stickySession()
+		} else {
+			l.currentUrl = l.roundRobin()
+		}
+
+		l.proxyRequest()
+	}
+}
+
+func main() {
+	var rawURLs = []string{"http://127.0.0.1:8081", "http://127.0.0.1:8082"}
+
+	loadBalancer := LoadBalancer{
+		stickySessionEnabled: true,
+	}
+
+	loadBalancer.parseUrls(rawURLs)
+
 	r := gin.Default()
 
-	r.Any("/*path", proxyRequest(urls))
+	r.Any("/*path", loadBalancer.handleRequest())
 
 	r.Run()
 }
