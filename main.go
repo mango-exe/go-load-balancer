0679cf6aa43432240646ee36e8606d198b81c85e
albert-emilian <albert.ion@nextlab.tech>
2024-06-27 16:27:40 +0300

    feat: sticky sessions

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"encoding/base64"
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 	"sync"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 var (
@@ -14,10 +18,41 @@ var (
 	mutex   sync.Mutex
 )
 
+type StickySession struct {
+	cookies []*http.Cookie
+}
+
 func proxyRequest(urls []*url.URL) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		existingCookie, err := ctx.Request.Cookie("sticky-session")
+
 		currentUrl := roundRobin(urls)
-		fmt.Fprintf(gin.DefaultWriter, currentUrl.Host)
+
+		if err != nil {
+			id := uuid.New()
+			cookieValue := fmt.Sprintf("%s@%s", id, currentUrl.String())
+			encodedValue := base64.RawStdEncoding.EncodeToString([]byte(cookieValue))
+
+			ctx.SetCookie("sticky-session", encodedValue, 3600, "", "", false, true)
+		} else {
+			decodedCookieBytes, err := base64.RawStdEncoding.DecodeString(existingCookie.Value)
+
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			splitCookieValue := strings.Split(string(decodedCookieBytes), "@")
+
+			if len(splitCookieValue) == 2 {
+				_, rawUrl := splitCookieValue[0], splitCookieValue[1]
+
+				currentUrl, err = url.Parse(rawUrl)
+
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
 
 		proxy := httputil.NewSingleHostReverseProxy(currentUrl)
 		ctx.Request.URL.Host = currentUrl.Host
